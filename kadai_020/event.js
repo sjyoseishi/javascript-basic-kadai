eec022a76fdfcb5efb87d1f66a0c87174338f943
sjyoseishi <yiting19841104@gmail.com>
2024-06-12 15:23:01 +0900

    kadai_020のプッシュ

---
+++ kadai_020/event.js
@@ -0,0 +1,12 @@
+//ボタンのid要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+//HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  //textというidを持つHTML要素を取得し、定数に代入する
+  const h2Value = document.getElementById('text');
+  //h2要素に「ボタンをクリックしました」というテキストを追加する
+  h2Value.textContent = "ボタンをクリックしました";
+  //「ボタンをクリックしました」というテキストを画面に表示する。
+  console.log(h2Value);
+});
